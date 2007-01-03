ef6bb953ba53dab7ea5f0a9d1aa273d8ba715db9
ssmoot <ssmoot@f5dac932-e207-0410-a3ba-84cc6beea89a>
2007-01-03 04:15:53 +0000

    Just a few more tweaks, including getting rid of the private Folder#get_folders method.
    git-svn-id: http://rexchange.rubyforge.org/svn/trunk@9 f5dac932-e207-0410-a3ba-84cc6beea89a

--- lib/rexchange/folder.rb
+++ lib/rexchange/folder.rb
@@ -7,31 +7,30 @@ require 'rexchange/appointment'
 
 module RExchange
 
+  class FolderNotFoundError < StandardError
+  end
+  
   class Folder
     include REXML
-    
-    include Enumerable
     
     attr_reader :credentails, :name
     
     def initialize(credentials, parent, name, content_type)
-      @credentials, @parent, @name = credentials, parent, name
-      @content_type = content_type
+      @credentials, @parent, @name, @content_type = credentials, parent, name, content_type
     end
     
-    alias :old_method_missing :method_missing
-    
-    # Used to access subfolders. If the subfolder does not
-    # exist, then old_method_missing is called.
+    # Used to access subfolders.
     def method_missing(sym, *args)
       if folders.has_key?(sym.to_s)
         folders[sym.to_s]
       else
-        old_method_missing(sym, args)
+        raise FolderNotFoundError.new("#{sym} is not a subfolder of #{name}")
       end
     end
     
-    # Iterate through each RExchange::Message in this folder
+    include Enumerable
+    
+    # Iterate through each entry in this folder
     def each
       content_type::find(@credentials, to_s).each do |item|
         yield item
@@ -53,48 +52,39 @@ module RExchange
     
     # Return an Array of subfolders for this folder
     def folders
-      @folders ||= get_folders
+      @folders ||= begin
+        request_body = <<-eos
+      		<?xml version="1.0"?>
+  				<D:searchrequest xmlns:D = "DAV:">
+  					 <D:sql>
+  					 SELECT "DAV:displayname", "DAV:contentclass"
+  					 FROM SCOPE('shallow traversal of "#{to_s}"')
+  					 WHERE "DAV:ishidden" = false
+                         AND "DAV:isfolder" = true
+  					 </D:sql>
+  				</D:searchrequest>
+        eos
+
+        response = DavSearchRequest.execute(@credentials, :body => request_body)
+
+        folders = {}
+
+        # iterate through folders query and add a new Folder
+        # object for each, under a normalized name.
+        xpath_query = "//a:propstat[a:status/text() = 'HTTP/1.1 200 OK']/a:prop"
+        Document.new(response.body).elements.each(xpath_query) do |m|
+          displayname = m.elements['a:displayname'].text
+          contentclass = m.elements['a:contentclass'].text
+          folders[displayname.normalize] = Folder.new(@credentials, self, displayname, contentclass.split(':').last)
+        end
+
+        folders
+      end
     end
     
     # Return the absolute path to this folder (but not the full URI)
     def to_s
       Folder.join(@parent, @name)
-    end
-    
-    private
-    
-    def get_folders
-      request_body = <<DA_QUERY
-    				<?xml version="1.0"?>
-    				<D:searchrequest xmlns:D = "DAV:">
-    					 <D:sql>
-    					 SELECT "DAV:displayname", "DAV:contentclass"
-    					 FROM SCOPE('shallow traversal of "#{to_s}"')
-    					 WHERE "DAV:ishidden" = false
-                           AND "DAV:isfolder" = true
-    					 </D:sql>
-    				</D:searchrequest>
-DA_QUERY
-      
-      response = DavSearchRequest.execute(@credentials, :body => request_body)
-      
-      folders = {}
-      
-      # iterate through folders query and add each normalized name to the folders array.
-      xpath_query = "//a:propstat[a:status/text() = 'HTTP/1.1 200 OK']/a:prop"
-      Document.new(response.body).elements.each(xpath_query) do |m|
-        
-        # p "#{m.elements['a:displayname'].text} : #{m.elements['a:contentclass'].text}"
-        # Mail folders like "inbox" are urn:content-classes:mailfolder types.
-        # Contacts folders like "contacts" are urn:content-classes:contactfolder types.
-        # I think this would be a better way to handle folder enumeration...
-        displayname = m.elements['a:displayname'].text
-        contentclass = m.elements['a:contentclass'].text
-        
-        folders[displayname.normalize] = Folder.new(@credentials, self, displayname, contentclass.split(':').last)
-      end
-      
-      return folders
     end
   end
 end
